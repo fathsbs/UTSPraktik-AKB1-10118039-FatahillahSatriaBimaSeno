47cc413f287a4ef3d585454a9e50000b27c2f030
fathsbs <fatahsbs@gmail.com>
2021-06-05 10:08:09 +0700

    Initial commit

---
+++ app/src/main/java/com/fathsbs/utspraktik_akb1_10118039_fatahillahsatriabimaseno/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fathsbs.utspraktik_akb1_10118039_fatahillahsatriabimaseno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
